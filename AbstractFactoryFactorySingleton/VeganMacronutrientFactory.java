8d38df85e02264cd6cd6b9d996b9f6a23967db1b
mbui2003 <126218902+mbui2003@users.noreply.github.com>
2024-03-14 22:34:10 -0700

    Update to Singleton

--- AbstractFactoryFactorySingleton/VeganMacronutrientFactory.java
+++ AbstractFactoryFactorySingleton/VeganMacronutrientFactory.java
@@ -1,7 +1,16 @@
-class VeganMacronutrientFactory implements MacronutrientAbstractFactory {
+public class VeganMacronutrientFactory implements MacronutrientAbstractFactory {
+    private static final VeganMacronutrientFactory instance = new VeganMacronutrientFactory();
+
     private CarbsFactory carbsFactory = VeganCarbsFactory.getInstance();
     private ProteinFactory proteinFactory = VeganProteinFactory.getInstance();
     private FatsFactory fatsFactory = VeganFatsFactory.getInstance();
+
+    private VeganMacronutrientFactory() {
+    }
+
+    public static VeganMacronutrientFactory getInstance() {
+        return instance;
+    }
 
     @Override
     public Carbs createCarbs() {
