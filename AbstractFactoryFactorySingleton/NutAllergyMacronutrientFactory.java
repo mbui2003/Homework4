b36b01312cc328bb6af32ff0a7acb9a960b4fca5
mbui2003 <126218902+mbui2003@users.noreply.github.com>
2024-03-14 22:34:27 -0700

    Update to Singleton

--- AbstractFactoryFactorySingleton/NutAllergyMacronutrientFactory.java
+++ AbstractFactoryFactorySingleton/NutAllergyMacronutrientFactory.java
@@ -1,7 +1,16 @@
-class NutAllergyMacronutrientFactory implements MacronutrientAbstractFactory {
+public class NutAllergyMacronutrientFactory implements MacronutrientAbstractFactory {
+    private static final NutAllergyMacronutrientFactory instance = new NutAllergyMacronutrientFactory();
+
     private CarbsFactory carbsFactory = NutAllergyCarbsFactory.getInstance();
     private ProteinFactory proteinFactory = NutAllergyProteinFactory.getInstance();
     private FatsFactory fatsFactory = NutAllergyFatsFactory.getInstance();
+
+    private NutAllergyMacronutrientFactory() {
+    }
+
+    public static NutAllergyMacronutrientFactory getInstance() {
+        return instance;
+    }
 
     @Override
     public Carbs createCarbs() {
